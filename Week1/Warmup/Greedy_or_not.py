4e7f2999e882763515ebdc0b270e7cc4008927c1
Bhakti_Jadhav <25b2199@iitb.ac.in>
2026-06-06 19:39:15 +0530

    Update Greedy_or_not.py

--- Week1/Warmup/Greedy_or_not.py
+++ Week1/Warmup/Greedy_or_not.py
@@ -1,1 +1,19 @@
-# Write your code here
+arr=[-5,5,9,-4,10,-9,0,3,-10]
+l=arr[0]
+r=arr[8]
+n=len(arr)
+
+def score(l,r):
+  if l==r:
+    return arr[l]
+  else:
+    take_left=arr[l]-score(l+1,r)
+    take_right=arr[r]-score(l,r-1)
+    return(max(take_left,take_right))#each player tries to maximise their score
+diff=score(0,n-1)#final diff of score of players
+if(diff>0):
+  print("Player 1 wins by",diff)
+elif(diff<0):
+  print("Player 2 wins by",-diff)
+else:
+  print("tie")
