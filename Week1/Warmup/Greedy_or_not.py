9f69c45b3a763b2c7f25391f1c8f99560605a4b9
Bhakti_Jadhav <25b2199@iitb.ac.in>
2026-07-04 23:31:15 +0530

    Greedy_or_not.py

--- Week1/Warmup/Greedy_or_not.py
+++ Week1/Warmup/Greedy_or_not.py
@@ -2,27 +2,25 @@ n=int(input())
 arr=[]
 for i in range(n):
   arr.append(int(input()))
+
 if n==0:
   print("tie")
-
-
-
-def score(l,r):
-  if l==r:
-    return arr[l]
-  else:
-    take_left=arr[l]-score(l+1,r)#my net advantage is my choice-opponents choice
-    take_right=arr[r]-score(l,r-1)
-    return(max(take_left,take_right))#get the max of the two whichever gives a higher diff score 
-diff=score(0,n-1)#final diff of score of players
-if(diff>0):
-  print("Player 1 wins by",diff)
-elif(diff<0):
-  print("Player 2 wins by",-diff)
 else:
-  print("tie")
+  def score(l,r):
+    if l==r:
+      return arr[l]
+    else:
+      take_left=arr[l]-score(l+1,r)#my net advantage is my choice-opponents choice
+      take_right=arr[r]-score(l,r-1)
+      return(max(take_left,take_right))#get the max of the two whichever gives a higher diff score
 
-
+  diff=score(0,n-1)#final diff of score of players
+  if(diff>0):
+    print("Player 1 wins by",diff)
+  elif(diff<0):
+    print("Player 2 wins by",-diff)
+  else:
+    print("tie")
 
 
     
