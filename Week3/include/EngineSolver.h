c3d8e22a772c62cd956bfff8f1b2e8171c383411
Bhakti_Jadhav <25b2199@iitb.ac.in>
2026-07-24 00:33:06 +0530

    Add files via upload

--- Week3/include/EngineSolver.h
+++ Week3/include/EngineSolver.h
@@ -4,6 +4,7 @@
 #include "../lib/chess/include/chess.hpp"
 #include <climits>  //for INT_MAX and INT_MIN
 #include <iostream> //for std::cout(debugging)
+#include <NNUE.h>
 
 class EngineSolver {
 private:
@@ -12,13 +13,15 @@ private:
     chess::Board board;           // stores the current chess position
     int maxDepth;                 // How deep to search (usually 4-6)/search depth
     long long nodesEvaluated;     // Counter for statistics/count the no of nodes evaluated/how many positions are evaluated
-    
-    
+    uint64_t hashKey;//as we need the hash for the current board position
+    NNUE nnue;
+    //the board must know hash of its current position 
     // ===== PRIVATE METHODS (Helper functions for internal use) =====
     //these are not directly called by the user/cannot be accessed by the user they are only used by the engine internally 
     //user doent call minimax,quiescence function they call the solver function which includes the use of these functions
     // Core minimax with alpha-beta pruning
     int minimax(int depth, int alpha, int beta, bool isMaximizing);
+    uint64_t generateHash();//the engine must calculate the initial hash value with the fen fed to it
     
     // Quiescence search to handle tactical positions
     int quiescence(int alpha, int beta,int depth_limit,bool ismaximizing);
@@ -26,6 +29,7 @@ private:
     // Check if a position is quiet (no captures available) to tell the quiescence search where to stop
     bool isQuiet();
     int evaluate();
+    
 
 public:
     // ===== CONSTRUCTOR DECLARATION=====
